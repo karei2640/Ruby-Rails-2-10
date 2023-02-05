9d4351e11883c64eda0cafcd8f215e4727ea183e
karei2640 <121594678+karei2640@users.noreply.github.com>
2023-02-05 06:12:01 +0000

    　Start2-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,45 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: "投稿に成功しました"
+    else
+      flash[:alert] = "投稿に失敗しました"
+      render :new
+    end
+  end 
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   def index
+    @book = Book.all
   end
 
   def show
+    @book = Book.find (params[:id])
+
   end
 
   def edit
+    @book = Book.find (params[:id])
+  end
+  
+ def update
+    book = Book.find(params[:id])
+    redirect_to books_path, notice: "更新に成功しました"
+    book.update(book_params)
+
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  post 'books'=>'books#create'
+  get 'books'=>'books#index'
+  get 'books/:id'=>'books#show',as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id'=>'books#destroy', as: 'destroy_book'
   get '/books' =>'books#top'
   get '/homes' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
