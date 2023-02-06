d23a95f7c2cdb69c01a61fc53b6989cfa7e2b341
karei2640 <121594678+karei2640@users.noreply.github.com>
2023-02-06 03:22:17 +0000

    [Start]2-10

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id'=>'books#destroy', as: 'destroy_book'
-  get '/books' =>'books#top'
+  get 'top' =>'books#new'
   get '/homes' => 'homes#top'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
